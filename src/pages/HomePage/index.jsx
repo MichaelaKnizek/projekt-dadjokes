fd7fd8e6fc53a3f33c650727202c4ebb0240c7ea
Michaela Knížková <michaela.knizek@gmail.com>
2025-05-31 19:29:47 +0200

    Add api jokes

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './style.css';
 import { Joke } from '../Joke/Joke';
 
 export const HomePage = () => {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch('http://localhost:4000/api/jokes');
+      const jsonData = await response.json();
+
+      // console.log(jsonData.data);
+
+      setJokes(jsonData.data);
+    };
+    getData();
+  }, []);
+
   return (
     <>
       <div className="container">
-        <Joke />
+        {jokes.map((joke) => {
+          return (
+            <Joke
+              key={joke.id}
+              userAvatar={joke.avatar}
+              username={joke.name}
+              text={joke.text}
+              likes={joke.likes}
+              dislikes={joke.dislikes}
+            />
+          );
+        })}
       </div>
     </>
   );
