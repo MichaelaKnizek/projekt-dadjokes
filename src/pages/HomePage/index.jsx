424e93f15d5c34a68fdf79c97b5b89b6b2691df7
Michaela Knížková <michaela.knizek@gmail.com>
2025-05-31 18:20:09 +0200

    Create Joke component including css and images

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,55 +1,13 @@
 import { useState } from 'react';
 import './style.css';
+import { Joke } from '../Joke/Joke';
 
 export const HomePage = () => {
-  const [likesUp, setLikesUp] = useState(0);
-  const [likesDown, setLikesDown] = useState(0);
-
-  const handleLikesUp = () => {
-    setLikesUp(likesUp + 1);
-  };
-
-  const handleLikesDown = () => {
-    setLikesDown(likesDown + 1);
-  };
-
   return (
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={handleLikesUp}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {likesUp}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={handleLikesDown}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {likesDown}
-          </span>
-        </div>
+    <>
+      <div className="container">
+        <Joke />
       </div>
-    </div>
+    </>
   );
 };
